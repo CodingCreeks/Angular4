b11ed1dc3e6ab2ba61fbb82f1f72d4412359e9da
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-03-26 12:02:00 -0400

    recipe-details renamed to recipe-detail

--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailsComponent } from './recipes/recipe-list/recipe-details/recipe-details.component';
+import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -17,7 +17,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeDetailsComponent,
+    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
