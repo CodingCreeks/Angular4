e976eccb7ce994d0d473bfc922c1256817d4e359
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-03-28 14:16:19 -0400

    property binding and set value from view page

--- Example/Directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ Example/Directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,26 +1,30 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private rederer: Renderer2) { }
 
   ngOnInit() {
     // this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor== this.defaultColor;
   }
 
   @HostListener('mouserenter') mouseover(eventdata: Event) {
     // this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouserleave') mouseleave(eventdata: Event) {
     // this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
