f17717c5a51d8899c4fa0d7624d9d99864d313ea
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-03-28 13:35:36 -0400

    Renderer is not working with @HostListener

--- Example/Directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ Example/Directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,7 +7,16 @@ export class BetterHighlightDirective implements OnInit {
 
   constructor(private elementRef: ElementRef, private rederer: Renderer2) { }
 
-    ngOnInit() {
-      this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    }
+  ngOnInit() {
+    // this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouserenter') mouseover(eventdata: Event) {
+    this.rederer.setStyle(this.elementRef.nativeElement, 'background-color','blue');
+  }
+
+  @HostListener('mouserleave') mouseleave(eventdata: Event) {
+    this.rederer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
+
 }
