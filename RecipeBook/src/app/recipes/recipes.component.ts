ffcc4d8dcdaea7ab183686dd5181e3e9d71f3936
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-03-29 12:32:56 -0400

    Using a Service for Cross-Component Communication and get ride of the Event and property Binding

--- RecipeBook/src/app/recipes/recipes.component.ts
+++ RecipeBook/src/app/recipes/recipes.component.ts
@@ -7,14 +7,17 @@ import { RecipeService } from "app/recipes/recipe.service";
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [ RecipeService ]
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => { this.selectedRecipe = recipe }
+    );
   }
 
 }
