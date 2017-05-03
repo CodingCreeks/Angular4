0c79ce341492f608484a5bf0a0590e2964c79e58
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-03 17:40:18 -0400

    Updating existing Items

--- RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -15,16 +15,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
-editedItem: Ingredient;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.subscription = this.shoppingListService.startedEditing.subscribe(
       (index: number) => {
-        this.editedItemIndex= index;
+        this.editedItemIndex = index;
         this.editMode = true;
-        this.editedItem= this.shoppingListService.getIngredient(index);
+        this.editedItem = this.shoppingListService.getIngredient(index);
         this.shoppingListForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount
@@ -36,7 +36,11 @@ editedItem: Ingredient;
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy() {
