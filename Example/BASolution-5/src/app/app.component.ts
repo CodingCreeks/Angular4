6825de790acf59b03fa5d0ddeec416e4d3431600
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-03 14:43:09 -0400

    completed

--- Example/BASolution-5/src/app/app.component.ts
+++ Example/BASolution-5/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('myForm') signUpForm: NgForm;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced';
 
+  userData = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+
+  onSubmit(form: NgForm) {
+    console.log(this.signUpForm.value);
+    this.userData.email = this.signUpForm.value.email;
+    this.userData.subscription = this.signUpForm.value.subscriptions;
+    this.userData.password = this.signUpForm.value.password;
+  }
 }
