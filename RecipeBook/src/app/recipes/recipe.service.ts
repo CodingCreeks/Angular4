8ae9185a42e4a6245962fd960c20dec15851f1aa
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-05 15:54:41 -0400

    GETing Back the Recipes from "FIREBASE" backend

--- RecipeBook/src/app/recipes/recipe.service.ts
+++ RecipeBook/src/app/recipes/recipe.service.ts
@@ -11,9 +11,9 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe(
-            'Summer Salad Recipe', 
-            'This is simply a test', 
-            'http://images.media-allrecipes.com/images/59094.jpg', 
+            'Summer Salad Recipe',
+            'This is simply a test',
+            'http://images.media-allrecipes.com/images/59094.jpg',
             [
                 new Ingredient('Watermelon', 2),
                 new Ingredient('Salt', 1),
@@ -21,8 +21,8 @@ export class RecipeService {
             ]),
 
         new Recipe(
-            'Winter Salad Recipe', 
-            'This is simply a test', 
+            'Winter Salad Recipe',
+            'This is simply a test',
             'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg',
             [
                 new Ingredient('Avocado', 2),
@@ -31,20 +31,20 @@ export class RecipeService {
             ]),
 
         new Recipe(
-            'Winter Salad Recipe', 
-            'This is simply a test', 
-            'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-13_mini.jpg', 
+            'Winter Salad Recipe',
+            'This is simply a test',
+            'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-13_mini.jpg',
             [
                 new Ingredient('Avocado', 2),
                 new Ingredient('Onions', 2),
                 new Ingredient('Green Chills', 2)
-    
+
             ]),
 
         new Recipe(
-            'Apple-Brussels Sprouts Salad', 
-            '1. In a small bowl whisk together vinegar 2. tbsp olive oil, honey, half the parsley, 1/2 tsp salt and 1/4 tsp black pepper. Set aside, Warm a small dry skillet over low heat, toast walnuts until lightly golden, about 1 to 2 minutes. 3. Toss together Brussels sprouts, apples and walnuts with half the dressing. Mix in additional dressing if desired. 4. Transfer to serving platter and top with grated Pecorino and remaining parsley. Serve immediately.', 
-            'http://del.h-cdn.co/assets/16/50/980x490/landscape-1481823535-unspecified-3.jpg', 
+            'Apple-Brussels Sprouts Salad',
+            '1. In a small bowl whisk together vinegar 2. tbsp olive oil, honey, half the parsley, 1/2 tsp salt and 1/4 tsp black pepper. Set aside, Warm a small dry skillet over low heat, toast walnuts until lightly golden, about 1 to 2 minutes. 3. Toss together Brussels sprouts, apples and walnuts with half the dressing. Mix in additional dressing if desired. 4. Transfer to serving platter and top with grated Pecorino and remaining parsley. Serve immediately.',
+            'http://del.h-cdn.co/assets/16/50/980x490/landscape-1481823535-unspecified-3.jpg',
             [
                 new Ingredient('Red wine vinegar', 2),
                 new Ingredient('Extra-virgin olive oil', 2),
@@ -57,9 +57,14 @@ export class RecipeService {
                 new Ingredient('Fuji apple, cored and thinly sliced', 1),
                 new Ingredient('Freshly grated pecorino', 1),
             ])
-        ];
+    ];
 
     constructor(private shoppingListService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
@@ -84,7 +89,7 @@ export class RecipeService {
     }
 
     deleteRecipe(index: number) {
-        this.recipes.splice(index,1);
+        this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
     }
 }
--- RecipeBook/src/app/shared/data-storage.service.ts
+++ RecipeBook/src/app/shared/data-storage.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
 
 import { RecipeService } from "app/recipes/recipe.service";
+import { Recipe } from "app/recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
@@ -10,6 +11,15 @@ export class DataStorageService {
 
     storeRecipes() {
         return this.http.put('https://recipebook-e33d8.firebaseio.com/recipes.json', this.recipeSerice.getRecipes());
+    }
+
+    getRecipes() {
+        return this.http.get('https://recipebook-e33d8.firebaseio.com/recipes.json')
+            .subscribe(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                this.recipeSerice.setRecipes(recipes);
+            });
     }
 
 }
