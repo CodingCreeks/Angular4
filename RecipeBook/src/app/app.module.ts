f29127358cbe0d6a4667fa1241aeb684aa3fae8f
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-08 17:37:17 -0400

    Restructuring Services to use the Child Injector

--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -4,11 +4,6 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from "app/shared/data-storage.service";
-import { AuthService } from "app/auth/auth.service";
-import { AuthGuard } from "app/auth/auth-guard.service";
 import { RecipesModule } from "app/recipes/recipes.module";
 import { ShoppingListModule } from "app/shopping-list/shopping-list.module";
 import { SharedModule } from "app/shared/shared.module";
@@ -29,7 +24,6 @@ import { CoreModule } from "app/core/core.module";
     AuthModule,
     CoreModule
   ],
-  providers: [ ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
