ab56ec42ee22d6f9403aa66e13b6f7434f213531
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-08 16:17:37 -0400

    Creating a Shopping List Feature Module

--- RecipeBook/src/app/app-routing.module.ts
+++ RecipeBook/src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from 'app/recipes/recipes.component';
 import { ShoppingListComponent } from "app/shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "app/recipes/recipe-start/recipe-start.component";
-import { RecipeDetailComponent } from "app/recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "app/recipes/recipe-edit/recipe-edit.component";
 import { SignupComponent } from "app/auth/signup/signup.component";
 import { SigninComponent } from "app/auth/signin/signin.component";
-import { AuthGuard } from "app/auth/auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from "app/shared/data-storage.service";
 import { SignupComponent } from './auth/signup/signup.component';
@@ -16,24 +13,23 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from "app/auth/auth.service";
 import { AuthGuard } from "app/auth/auth-guard.service";
 import { RecipesModule } from "app/recipes/recipes.module";
+import { ShoppingListModule } from "app/shopping-list/shopping-list.module";
 import { SharedModule } from "app/shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule,
     RecipesModule,
-    SharedModule
+    SharedModule,
+    ShoppingListModule
   ],
   providers: [ ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard ],
   bootstrap: [AppComponent]
