546821d39aa8f7794991a8dbdf9235731517e882
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-05-09 13:24:22 -0400

    AheadOfTime(AOT) problem resolved

--- RecipeBook/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ RecipeBook/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { FormGroup, FormControl, FormArray, Validators } from "@angular/forms";
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
-import { RecipeService } from "app/recipes/recipe.service";
+import { RecipeService } from 'app/recipes/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,7 +10,6 @@ import { RecipeService } from "app/recipes/recipe.service";
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
   id: number;
   editMode = false;
   recipeForm: FormGroup;
@@ -26,17 +25,15 @@ export class RecipeEditComponent implements OnInit {
         this.editMode = params['id'] != null;
         this.initForm();
       }
-    );
+      );
   }
 
   onSubmit() {
-
     // const newRecipe = new Recipe(
     //   this.recipeForm.value['name'],
     //   this.recipeForm.value['description'],
     //   this.recipeForm.value['imagePath'],
     //   this.recipeForm.value['ingredients']);
-
     if (this.editMode) {
       // this.recipeService.updateRecipe(this.id, newRecipe);
       //Advantage of Recative approch of passing the 'Form' values over the conventional approach
@@ -46,7 +43,6 @@ export class RecipeEditComponent implements OnInit {
       //Advantage of Recative approch of passing the 'Form' values over the conventional approach
       this.recipeService.addRecipe(this.recipeForm.value);
     }
-
     this.onCancel();
   }
 
@@ -55,19 +51,23 @@ export class RecipeEditComponent implements OnInit {
       new FormGroup({
         'name': new FormControl(null, Validators.required),
         'amount': new FormControl(null, [
-          Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
         ])
       })
     );
   }
-
 
   onDeleteIngredient(index: number) {
     (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
 
   onCancel() {
-    this.router.navigate(['../'], { relativeTo: this.route })
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
   private initForm() {
@@ -81,14 +81,14 @@ export class RecipeEditComponent implements OnInit {
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
-
       if (recipe['ingredients']) {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
               'name': new FormControl(ingredient.name, Validators.required),
               'amount': new FormControl(ingredient.amount, [
-                Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/)
               ])
             })
           );
