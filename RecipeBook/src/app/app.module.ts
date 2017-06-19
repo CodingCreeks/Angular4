3352a8a8dbe86a4676bc96a54fe68ea8d6012ce8
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-06-19 13:01:06 -0400

    added the home, about component and routing to the project

--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { ShoppingListModule } from "app/shopping-list/shopping-list.module";
 import { SharedModule } from "app/shared/shared.module";
 import { AuthModule } from "app/auth/auth.module";
 import { CoreModule } from "app/core/core.module";
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
--- a2Website/src/app/app.module.ts
+++ a2Website/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from "app/app-routing.module";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { HomeComponent } from "app/pages/home/home.component";
+import { AboutComponent } from "app/pages/about/about.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    JumbotronComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
