8218547fd4e7f8789ae1416188e43082535860b5
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-06-28 13:37:36 -0400

    header renamed to home

--- yarn/hello-yarn/src/app/app.module.ts
+++ yarn/hello-yarn/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from "./components/header/navbar/navbar.component";
+import { NavbarComponent } from "./components/home/navbar/navbar.component";
 
 
 @NgModule({
