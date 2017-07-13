7750cbe54e0121274618075f7a925cdea873190b
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-13 15:21:47 -0400

    added the dummy data in service

--- phonebook/src/app/contacts.service.ts
+++ phonebook/src/app/contacts.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface Contact {
+  name: string;
+  number: string;
+}
+
 @Injectable()
 export class ContactsService {
 
   constructor() { }
 
+  getContacts() {
+    let contacts: Contact[] = [
+      {
+        name: "Ram Surya",
+        number: "123456575585"
+      },
+      {
+        name: "Sam Surya",
+        number: "456565655685"
+      },
+      {
+        name: "Ravi Surya",
+        number: "575756658787"
+      }
+    ];
+    return contacts;
+  }
 }
