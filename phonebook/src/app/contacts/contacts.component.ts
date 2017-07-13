4e0f5d0bfa26d7c22c38f66977a5eef961317aef
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-13 16:05:19 -0400

    display the contact

--- phonebook/src/app/contacts/contacts.component.ts
+++ phonebook/src/app/contacts/contacts.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ContactsService } from './../contacts.service';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  contacts: any[];
+  constructor(private contactService: ContactsService) { }
 
   ngOnInit() {
+    this.contacts = this.contactService.getContacts();
   }
 
 }
