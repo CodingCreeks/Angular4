ac43efdfd71216443648b5c1c7f3a739857a5334
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-14 15:37:03 -0400

    added the components to the application

--- ng4-styles-animations/src/app/app.module.ts
+++ ng4-styles-animations/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewProjectComponent,
+    ProjectComponent,
+    ProjectsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
