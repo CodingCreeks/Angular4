3697bbd1fa30ace5a95247b2ed824ad7d6a843a3
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-15 12:45:17 -0400

    dynamic transitions of the elements

--- ng4-styles-animation-module4/src/app/app.component.ts
+++ ng4-styles-animation-module4/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
-import { showStateTrigger } from './animations';
+import { showStateTrigger, animateStateTrigger } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    showStateTrigger
+    showStateTrigger,
+    animateStateTrigger
   ]
 })
 export class AppComponent {
   isShown = false;
+  shrikWidth = 500;
+  animate = false;
 
   showParagraph() {
     this.isShown = !this.isShown;
