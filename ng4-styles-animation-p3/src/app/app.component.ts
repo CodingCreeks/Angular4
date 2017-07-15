03673f14130d67bd464188cf679fb84387aea596
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-15 03:22:43 -0400

    Outsourced the animations

--- ng4-styles-animation-p3/src/app/app.component.ts
+++ ng4-styles-animation-p3/src/app/app.component.ts
@@ -1,59 +1,12 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from "@angular/animations";
+import { numberEnteredStateTrigger, clickedStateTrigger } from './animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('clickedState', [
-      state('default', style({
-        backgroundColor: 'orange',
-        width: '100px',
-        height: '100px'
-      })),
-      state('clicked', style({
-        backgroundColor: 'blue',
-        width: '500px',
-        height: '75px'
-      })),
-      state('mousedown', style({
-        backgroundColor: 'red',
-        border: '1px solid black',
-        width: '100px',
-        height: '100px'
-      })),
-      transition('default <=>clicked', animate('1s 500ms ease-in')),
-      // transition('clicked => default', animate('300ms ease-out'))
-      // transition('clicked => default', animate(300)),
-      transition('mousedown <=> clicked', animate(300)),
-      // transition('clicked => mousedown', animate(300))
-    ]),
-    trigger('numberEnteredState', [
-      state('unselected', style({
-        border: '1px solid black',
-        padding: '5px',
-        backgroundColor: 'white'
-      })),
-      state('selected', style({
-        border: '2px solid blue',
-        padding: '4px',
-        backgroundColor: 'lightblue'
-      })),
-      transition('unselected => selected', [
-        style({
-          border: '2px solid black',
-          padding: '4px',
-          transform: 'scale(1)'
-        }),
-        animate('600ms 100ms ease-out', style({
-          backgroundColor: 'red',
-          transform: 'scale(1.05)'
-        })),
-        animate(300)
-      ])
-    ])
-  ]
+  animations: [clickedStateTrigger, numberEnteredStateTrigger]
 })
 export class AppComponent {
   clickInfo = 'default';
