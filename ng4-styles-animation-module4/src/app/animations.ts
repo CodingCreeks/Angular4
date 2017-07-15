2bdd1d3c8239c31296f30248b043a07814bda7f7
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-15 11:39:59 -0400

    alias for common transitions

--- ng4-styles-animation-module4/src/app/animations.ts
+++ ng4-styles-animation-module4/src/app/animations.ts
@@ -1,13 +1,25 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 export const showStateTrigger = trigger('showState', [
-  transition('void => *', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300)
+  // ]),
+  // transition('* => void', [
+  //   style({
+  //     opacity: 0
+  //   })
+  // ])
+
+  transition(':enter', [
     style({
       opacity: 0
     }),
     animate(300)
   ]),
-  transition('* => void', [
+  transition(':leave', [
     style({
       opacity: 0
     })
