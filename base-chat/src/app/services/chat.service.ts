b62a5605024252c7e535e0aca7ff33c74ef4da1b
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-08-29 13:44:07 -0400

    update the html and  created the models for the project

--- base-chat/src/app/services/chat.service.ts
+++ base-chat/src/app/services/chat.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import{ AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/Auth';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../services/auth.service';
+import * as firbase from 'firebase/app';
+
+import{ ChatMessage} from '../models/chat-message.model';
+
 @Injectable()
 export class ChatService {
 
   constructor() { }
 
+  sendMessage(){
+
+  }
 }
